84ed1a31e5124f5d500ea49109c675eb1fd07ac9
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-08-25 12:10:54 -0400

    finished the refactor assignment for album and artist views

--- src/components/views/AlbumView.js
+++ src/components/views/AlbumView.js
@@ -5,13 +5,33 @@ function AlbumView() {
     const { id } = useParams()
     const [albumData, setAlbumData] = useState([])
 
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/song/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justSongs = albumData.filter(entry => entry.wrapperType === 'track')
+
+    const renderSongs = justSongs.map((song, i) => {
+        return (
+            <div key={i}>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
+
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Album Data Goes Here!</p>
+            {renderSongs}
         </div>
     )
 }
+
 
 
 export default AlbumView
--- src/components/views/ArtistView.js
+++ src/components/views/ArtistView.js
@@ -1,40 +1,78 @@
+// import { useState, useEffect } from 'react'
+// import { useParams, Link, useNavigate } from 'react-router-dom'
+
+// function ArtistView() {
+//     const { id } = useParams()
+//     const [artistData, setArtistData] = useState([])
+//     let [justAlbums, setJustAlbums] = useState([])
+//     useEffect(() => {
+//         const API_URL = `http://localhost:4000/album/${id}`
+//         const fetchData = async () => {
+//             const response = await fetch(API_URL)
+//             const resData = await response.json()
+//             setArtistData(resData.results)
+
+//         }
+//         fetchData()
+//     }, [id])
+//     setJustAlbums(artistData.filter(entry => entry.collectionType === 'Album'))
+
+//     console.log('just albums', justAlbums, 'artist data here', artistData)
+
+//     const renderAlbums = justAlbums.map((album, i) => {
+//         return (
+//             <div key={i}>
+//                 <p>{album.collectionName}</p>
+//             </div>
+//         )
+//     })
+
+//     return (
+//         <div>
+//             <h2>The id passed was: {id}</h2>
+//             <p>Artist Data Goes Here!</p>
+//             {renderAlbums}
+//         </div>
+//     )
+
+// }
+
 import { useState, useEffect } from 'react'
-import { useParams, Link, useNavigate } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 
 function ArtistView() {
     const { id } = useParams()
     const [artistData, setArtistData] = useState([])
-    let [justAlbums, setJustAlbums] = useState([])
+
     useEffect(() => {
         const API_URL = `http://localhost:4000/album/${id}`
         const fetchData = async () => {
             const response = await fetch(API_URL)
             const resData = await response.json()
             setArtistData(resData.results)
-            setJustAlbums(artistData.filter(entry => entry.collectionType === 'Album'))
         }
         fetchData()
     }, [id])
-    // const justAlbums = artistData.filter(entry => entry.kind === 'album')
 
-    console.log(justAlbums, artistData)
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
 
     const renderAlbums = justAlbums.map((album, i) => {
         return (
             <div key={i}>
-                <p>{album.collectionName}</p>
+                <Link to={`/album/${album.collectionId}`}>
+                    <p>{album.collectionName}</p>
+                </Link>
             </div>
         )
     })
-
     return (
         <div>
             <h2>The id passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
+            <p>Artist Data Goes here</p>
             {renderAlbums}
         </div>
     )
-
 }
+
 
 export default ArtistView
