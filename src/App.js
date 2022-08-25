986795308977b1a3a2c4e7e5e7b2211a5760defd
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-08-25 11:49:16 -0400

    switching branches

--- src/App.js
+++ src/App.js
@@ -12,25 +12,19 @@ function App() {
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
-  useEffect(() => {
-    if (search) {
-      const fetchData = async () => {
-        document.title = `${search} Music`
-        const response = await fetch(API_URL + search)
-        const resData = await response.json()
-        if (resData.results.length > 0) {
-          setData(resData.results)
-        } else {
-          setMessage('Not Found')
-        }
-      }
-      fetchData()
-    }
-  }, [search])
-
   const handleSearch = (e, term) => {
     e.preventDefault()
-    setSearch(term)
+    const fetchData = async () => {
+      document.title = `${term} Music`
+      const response = await fetch(API_URL + term)
+      const resData = await response.json()
+      if (resData.results.length > 0) {
+        setData(resData.results)
+      } else {
+        setMessage('Not Found')
+      }
+    }
+    fetchData()
   }
 
   return (
