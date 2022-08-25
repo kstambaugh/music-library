5e3e041fcfccc1544578a51952efaf83537db796
kstambaugh <kyle.w.stambaugh1@gmail.com>
2022-08-25 11:45:07 -0400

    refactoring with useref

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import { DataContext } from './context/DataContext'
 import Gallery from './components/gallery'
 import SearchBar from './components/searchBar'
+import { SearchContext } from './context/SearchContext'
 
 function App() {
   let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
+  let searchInput = useRef('')
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
@@ -32,9 +35,16 @@ function App() {
 
   return (
     <div>
-      <SearchBar handleSearch={handleSearch} />
+      <SearchContext.Provider value={{
+        term: searchInput,
+        handleSearch: handleSearch
+      }}>
+        <SearchBar handleSearch={handleSearch} />
+      </SearchContext.Provider>
       {message}
-      <Gallery data={data} />
+      <DataContext.Provider value={data}>
+        <Gallery data={data} />
+      </DataContext.Provider>
     </div>
   )
 }
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,14 +1,13 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { SearchContext } from '../context/SearchContext'
 
-function SearchBar(props) {
-    let [searchTerm, setSearchTerm] = useState('')
+function SearchBar() {
+    const { term, handleSearch } = useContext(SearchContext)
 
     return (
-        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-
-            <input type='text' placeholder='Enter a search term here'
-                onChange={(e) => setSearchTerm(e.target.value)} />
-            <input type='submit' />
+        <form>
+            <input ref={term} type='text' placeholder='Enter a search term here' />
+            <button onClick={(e) => handleSearch(e, term.current.value)} type='submit' ></button>
         </form>
     )
 }
